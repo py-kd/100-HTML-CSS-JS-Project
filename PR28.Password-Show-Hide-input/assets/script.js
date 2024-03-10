16ccb573d4aec3760eb2633c4e5b6c27bab80de1
bd <patelbapu2000@gmail.com>
2024-03-10 21:47:58 +0530

    'Password-Show-Hide'

---
+++ PR28.Password-Show-Hide-input/assets/script.js
@@ -0,0 +1,17 @@
+const input = document.querySelector('#input-box');
+console.log(input);
+
+const eye = document.querySelector('#eye-icon');
+console.log(eye);
+
+eye.addEventListener('click', () =>{      
+      // if (input.type === 'password'){
+      //       input.type = 'text';
+      // }      else{
+      //       input.type = 'password';
+      // }
+
+      // REFECTOR 
+      input.type = input.type === 'password' ? 'text' : 'password';
+      input.type === "password" ? (input.type = 'text') : (input.type = 'password');
+})
